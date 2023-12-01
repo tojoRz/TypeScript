3000f3a69d0ccb1dd2a92dd204765a5def1271cc
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2023-12-01 11:45:55 +0300

    Update app.ts

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,7 @@
-console.log('Hello');
+var compteur = document.querySelector('#compteur');
+var i = 0;
+var increment = function (e) {
+    i++;
+    compteur.querySelector('span').innerText = i.toString();
+};
+compteur.addEventListener('click', increment);
