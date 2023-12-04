72636a1ce32a761a92819d74a909d79c65149e6c
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2023-12-04 10:30:20 +0300

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -4,9 +4,9 @@ let i = 0;
 const increment = (e) => {
     e.preventDefault();
     i++;
-    const span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector('span');
+    const span = compteur?.querySelector('span');
     if (span) {
         span.innerText = i.toString();
     }
 };
-compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener('click', increment);
+compteur?.addEventListener('click', increment);
