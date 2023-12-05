4ac2bacbe03afbc8ee9814ff82bf94c8fabd833c
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2023-12-05 11:17:25 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -12,39 +12,77 @@ const increment = (e: Event) => {
 
 compteur?.addEventListener("click", increment);
 
-//Les classes en typeScript
+/**
+ * !Les classes en typeScript
+ * ? Exemple
+*/
+// function reverse<T>(arr: readonly T[]): T[] {
+//     return [...arr].reverse();
+//   }
+  
+//   class A {
+//     constructor(public a: number) {}
+//   }
+  
+//   const aInstance = new A(3);
+//   console.log(aInstance.a);
+  
+//   class Collection<T> {
+//     constructor(private items: T[]) {}
+  
+//     add(items: T) {
+//       this.items.push(items);
+//       return this;
+//     }
+  
+//     first(): T | null {
+//       return this.items[0] || null;
+//     }
+  
+//     isEqual(a: this) {
+//       return a.items === this.items ;
+//     }
+//   }
+  
+//   class SubCollection<T> extends Collection<T> {
+//       a =3
+//   }
+  
+//   class Subscriber {
+//     on = (name: string, cb: Function) => {};
+//   }
+  
+//   const a = new Collection([1, 2]);
+//   const b = new Collection([2, 2]);
+//   a.isEqual(b);
+// class Point {
+//     x = 0
+//     y = 0
+// }
 
-function reverse<T>(arr: readonly T[]): T[] {
-  return [...arr].reverse();
+// class Geometry {
+//     x = 0
+//     y = 0
+//     surface = 0
+// }
+
+// function getX(p: Point) {
+//     return p.x
+// }
+
+// getX(new Geometry());
+
+abstract class Geometry {
+    x = 0
+    y = 0
+    abstract surface(): number
 }
 
-class A {
-  constructor(public a: number) {}
+class Triangle extends Geometry {
+    x = 2
+    y = 2
+
+    surface(): number {
+        return 3
+    }
 }
-
-const aInstance = new A(3);
-console.log(aInstance.a);
-
-class Collection<T> {
-  constructor(private items: T[]) {}
-
-  add(items: T) {
-    this.items.push(items);
-    return this;
-  }
-
-  first(): T | null {
-    return this.items[0] || null;
-  }
-
-  isEqual(a: this) {
-    return;
-  }
-}
-
-class Subscriber {
-  on = (name: string, cb: Function) => {};
-}
-
-const a = new Collection([1, 2]);
-const b = a.first();
