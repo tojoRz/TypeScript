be3a5375ef6fe0b5b3b06950723e93bdaa27b6d0
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2023-12-05 10:28:14 +0300

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,45 @@
 "use strict";
-const compteur = document.querySelector('#compteur');
+const compteur = document.querySelector("#compteur");
 let i = 0;
 const increment = (e) => {
     e.preventDefault();
     i++;
-    const span = compteur?.querySelector('span');
+    const span = compteur?.querySelector("span");
     if (span) {
         span.innerText = i.toString();
     }
 };
-compteur?.addEventListener('click', increment);
+compteur?.addEventListener("click", increment);
+//Les classes en typeScript
+function reverse(arr) {
+    return [...arr].reverse();
+}
+class A {
+    a;
+    constructor(a) {
+        this.a = a;
+    }
+}
+const aInstance = new A(3);
+console.log(aInstance.a);
+class Collection {
+    items;
+    constructor(items) {
+        this.items = items;
+    }
+    add(items) {
+        this.items.push(items);
+        return this;
+    }
+    first() {
+        return this.items[0] || null;
+    }
+    isEqual(a) {
+        return;
+    }
+}
+class Subscriber {
+    on = (name, cb) => { };
+}
+const a = new Collection([1, 2]);
+const b = a.first();
