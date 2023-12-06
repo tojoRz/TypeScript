ddade62e10d20fb892b24139d6a25125f083a500
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2023-12-06 10:00:10 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -87,10 +87,51 @@ compteur?.addEventListener("click", increment);
 //     }
 // }
 
-class Geometry {
-    static #origin: {x: number, y: number}
+// class Geometry {
+//     static #origin: {x: number, y: number}
 
-    static {
-        Geometry.#origin = {x: 0, y:0}
-    }
-}
+//     static {
+//         Geometry.#origin = {x: 0, y:0}
+//     }
+// }
+
+// class Geometry {
+//   static #origin: {x: number, y: number}
+
+//   constructor(x: number, y: number) {
+    
+//   }
+
+//   surface() {
+//     return 6
+//   }
+// }
+
+// class Triangle {
+//   constructor(x: number, y: number) {
+    
+//   }
+
+//   surface() {
+//     return 3
+//   }
+// }
+
+// type InstantiableShape = {
+//   new (x: number, y:number): {
+//     surface: () => number
+//   }
+// }
+
+// function shapeGenerator(shapeType: InstantiableShape, x: number, y:number){
+//   return new shapeType(x, y).surface()
+// }
+
+// shapeGenerator(Geometry, 10, 20)
+// shapeGenerator(Triangle, 10, 20)
+
+
+/**
+ * !Type vs Interface
+ * ? Exemple
+*/
