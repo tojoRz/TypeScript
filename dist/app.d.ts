cf0abe0ed155e4fb85c83ea66e095444726bb0c4
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2023-12-08 14:19:32 +0300

    Update app.d.ts

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -1,3 +1,6 @@
+declare const compteur: Element | null;
+declare let i: number;
+declare const increment: (e: Event) => void;
 /**
  * !Les classes en typeScript
  * ? Exemple
@@ -21,8 +24,59 @@
  * !Les fichiers de declaration
  * ? Exemple
 */
-export declare class Point {
-    x: number;
-    y: number;
-    move(x: number, y: number): this;
+/**
+ * !Types utilitaires
+ * ? Exemple
+*/
+/**
+ * class Poisons {
+
 }
+
+class Chat {
+
+}
+
+
+type AnimalOptions = { nager: any } | { sauter: any }
+type AnimalFromOption<T> = T extends {nager: any} ? Poisons : Chat
+
+function generator<T extends AnimalOptions >( options: T): AnimalFromOption<T>
+{
+  if ("nager" in options ){
+    return new Poisons()
+  } else {
+    return new Chat()
+  }
+}
+
+const a = generator({nager:'aze'})
+ */
+/**
+ * class Poisons {
+  cri () {
+    return false
+  }
+}
+
+class Chat {
+  cri () {
+    return 'miaouu'
+  }
+}
+
+type AnimalCri<T> = T extends { cri : () => infer U} ? U : never
+
+type A = AnimalCri<Poisons>
+type B = AnimalCri<Chat>
+ */
+declare class FeatureFlags {
+    env: string;
+    darkMode(): boolean;
+    privateMode(): boolean;
+    nfswMode(): boolean;
+}
+type OptionFlags<T> = {
+    +readonly [key in keyof T as `get${string & key}`]: T[key] extends () => boolean ? boolean : never;
+};
+type A = OptionFlags<FeatureFlags>;
