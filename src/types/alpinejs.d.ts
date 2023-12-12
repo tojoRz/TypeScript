510360fa03c32d79eb16da254ebe1576bf9c01ee
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2023-12-12 11:16:53 +0300

    Create alpinejs.d.ts

---
+++ src/types/alpinejs.d.ts
@@ -0,0 +1,17 @@
+declare module "alpinejs" {
+
+  type AlpineInstance = {
+    readonly $el: HTMLElement;
+  };
+
+  type WithAlpineInstance<T> = {
+    [key in keyof T]: T[key] extends (...args: infer Args) => infer Return ? (this: T & AlpineInstance, ...args: Args) => Return : T[key]
+  }
+
+  const Alpine: {
+    start(): void;
+    data<T>(componentName: string, callBack: (params: any) => T): void;
+  };
+
+  export default Alpine;
+}
